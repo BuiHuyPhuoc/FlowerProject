6f68ad9cdf61840b4ecc8c417ad80d1908f8286c
BuiHuyPhuoc <buihuyphuoc123@gmail.com>
2023-03-26 04:59:46 +0700

    Update: Bộ lọc theo Cate

--- app/src/main/java/com/example/appdemo/model/SanPhamMoi.java
+++ app/src/main/java/com/example/appdemo/model/SanPhamMoi.java
@@ -7,7 +7,7 @@ public class SanPhamMoi implements Serializable {
     Integer SOLUONG, HINHANH;
     Long DONGIA;
     public SanPhamMoi(){ }
-    public SanPhamMoi(String MASP, String TENSP, String PHANLOAI, String NOINHAP, String NOIDUNG,Integer SOLUONG, Long DONGIA, Integer HINHANH) {
+    public SanPhamMoi(String MASP, String TENSP, String PHANLOAI, Integer SOLUONG, String NOINHAP, String NOIDUNG, Long DONGIA, Integer HINHANH) {
         this.MASP = MASP;
         this.TENSP = TENSP;
         this.PHANLOAI = PHANLOAI;
