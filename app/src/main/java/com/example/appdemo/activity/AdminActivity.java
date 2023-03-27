e5aceb9c808367fa6100832f7a707daf0e663fbf
anhnhatcc700 <114375962+anhnhatcc700@users.noreply.github.com>
2023-03-27 21:17:10 +0700

    test

---
+++ app/src/main/java/com/example/appdemo/activity/AdminActivity.java
@@ -0,0 +1,16 @@
+package com.example.appdemo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appdemo.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
