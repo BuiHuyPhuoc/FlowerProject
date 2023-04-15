91360d2d7c488a532660d0abb18189a704767269
VoQuiNam <116937359+VoQuiNam@users.noreply.github.com>
2023-04-15 23:11:16 +0700

    update

--- app/src/main/java/com/example/appdemo/activity/ChiTietSPActivity.java
+++ app/src/main/java/com/example/appdemo/activity/ChiTietSPActivity.java
@@ -3,10 +3,12 @@ package com.example.appdemo.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -48,23 +50,25 @@ public class ChiTietSPActivity extends AppCompatActivity {
     }
 
     private void themGioHang() {
+
         if (Utils.manggiohang.size() > 0){
             boolean flag = false;
             int sl = Integer.parseInt(spinner.getSelectedItem().toString());
+            long gia = sanPhamMoi.getDONGIA() * sl;
             for (Integer i = 0 ; i < Utils.manggiohang.size() ; i++)
             {
                 //Truong72 hop trung sp
                if (Utils.manggiohang.get(i).getIdsp() == sanPhamMoi.getMASP())
                {
                    Utils.manggiohang.get(i).setSoluong(sl + Utils.manggiohang.get(i).getSoluong());
-                   long gia = sanPhamMoi.getDONGIA() * Utils.manggiohang.get(i).getSoluong();
+                   gia = sanPhamMoi.getDONGIA() * Utils.manggiohang.get(i).getSoluong();
                    Utils.manggiohang.get(i).setDONGIA(gia);
                    flag = true;
                }
             }
             if(flag == false)
             {
-                long gia = sanPhamMoi.getDONGIA() * sl;
+
                 GioHang gioHang = new GioHang();
                 gioHang.setDONGIA(gia);
                 gioHang.setSoluong(sl);
@@ -86,7 +90,11 @@ public class ChiTietSPActivity extends AppCompatActivity {
             Utils.manggiohang.add(gioHang);
 
         }
-        badge.setText(String.valueOf(Utils.manggiohang.size()));
+        int toTalItem = 0;
+        for (int i = 0 ; i < Utils.manggiohang.size() ; i++){
+            toTalItem = toTalItem + Utils.manggiohang.get(i).getSoluong();
+        }
+        badge.setText(String.valueOf((toTalItem)));
     }
 
     private void intData(){
@@ -95,7 +103,7 @@ public class ChiTietSPActivity extends AppCompatActivity {
         motasp.setText(sanPhamMoi.getNOIDUNG());
         Glide.with(getApplicationContext()).load(sanPhamMoi.getHINHANH()).into(imgHinhAnh);
         DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
-        giasp.setText("Giá: "+decimalFormat.format(Double.parseDouble(String.valueOf(sanPhamMoi.getDONGIA()))) + " VNĐ");
+        giasp.setText("Giá: "+decimalFormat.format(Double.parseDouble(String.valueOf(sanPhamMoi.getDONGIA()))) + " Đ");
         Integer[] sl = new Integer[]{1,2,3,4,5,6,7,8,9,10};
         ArrayAdapter<Integer> adapterspin = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,sl);
         spinner.setAdapter(adapterspin);
@@ -110,8 +118,20 @@ public class ChiTietSPActivity extends AppCompatActivity {
         imgHinhAnh = (ImageView) findViewById(R.id.imgchitiet);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         badge = (NotificationBadge) findViewById(R.id.menu_sl);
+        FrameLayout frameLayoutgiohang = findViewById(R.id.framegiohang);
+        frameLayoutgiohang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent giohang = new Intent(getApplicationContext(),GioHangActivity.class);
+                startActivity(giohang);
+            }
+        });
         if (Utils.manggiohang != null){
-            badge.setText(String.valueOf(Utils.manggiohang.size()));
+            int toTalItem = 0;
+            for (int i = 0 ; i < Utils.manggiohang.size() ; i++){
+                toTalItem = toTalItem + Utils.manggiohang.get(i).getSoluong();
+            }
+            badge.setText(String.valueOf(toTalItem));
         }
     }
 
@@ -124,5 +144,19 @@ public class ChiTietSPActivity extends AppCompatActivity {
                finish();
             }
         });
+    }
+
+    //cập nhật lại giỏ hàng sau khi xóa sp khỏi giỏ hàng
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Utils.manggiohang != null){
+            int toTalItem = 0;
+            for (int i = 0 ; i < Utils.manggiohang.size() ; i++){
+                toTalItem = toTalItem + Utils.manggiohang.get(i).getSoluong();
+            }
+            badge.setText(String.valueOf(toTalItem));
+        }
     }
 }
--- app/src/main/java/com/example/appdemo/activity/GioHangActivity.java
+++ app/src/main/java/com/example/appdemo/activity/GioHangActivity.java
@@ -1,16 +1,108 @@
 package com.example.appdemo.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
 
 import com.example.appdemo.R;
+import com.example.appdemo.Utils;
+import com.example.appdemo.adapter.GioHangAdapter;
+import com.example.appdemo.model.EvenBus.TinhTongEvent;
+import com.example.appdemo.model.GioHang;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.text.DecimalFormat;
+import java.util.List;
 
 public class GioHangActivity extends AppCompatActivity {
+    //123
+    TextView giohangtrong,tongtien;
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    Button btnmuahang;
+    GioHangAdapter adapter;
+    List<GioHang> gioHangList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gio_hang);
+        intView();
+        intControl();
+        totalMoney();
+    }
+
+    private void totalMoney() {
+        long tongtiensp = 0;
+        for (int i = 0 ; i < Utils.manggiohang.size() ; i++){
+            tongtiensp = tongtiensp + (Utils.manggiohang.get(i).getDONGIA() * Utils.manggiohang.get(i).getSoluong());
+        }
+        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+      tongtien.setText(decimalFormat.format(tongtiensp) + " VNĐ ");
+    }
+
+
+    private void intControl() {
+        setSupportActionBar(toolbar);//lay doi tuong actonbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//bat cai icon len
+
+        //Click vào nút icon quay lại
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);//LayoutManager: xác định ra vị trí của các item trong RecyclerView.
+        recyclerView.setLayoutManager(layoutManager);
+        //trường hợp giỏ hàng trống
+        if (Utils.manggiohang.size() == 0){
+            giohangtrong.setVisibility(View.VISIBLE);
+        }
+        else {
+            adapter = new GioHangAdapter(getApplicationContext(),Utils.manggiohang);
+            recyclerView.setAdapter(adapter);
+        }
+    }
+
+    private void intView(){
+        giohangtrong = findViewById(R.id.tvGioHangTrong);
+        toolbar = findViewById(R.id.toolbar);
+        recyclerView = findViewById(R.id.recycleViewgiohang);
+        tongtien = findViewById(R.id.tongtien);
+        btnmuahang = findViewById(R.id.btnMuahang);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);//đăng ký lắng nghe sk
+    }
+
+    @Override
+    protected void onStop() {
+        EventBus.getDefault().unregister(this);
+        super.onStop();
+    }
+
+    //ThreadMode:sẽ chạy trên luồng nào và nó sẽ cập nhật gì thì chạy luồng này
+    @Subscribe(sticky = true,threadMode = ThreadMode.MAIN)
+    public void eventTinhTien(TinhTongEvent event){
+        if (event != null){
+            totalMoney();
+        }
     }
 }
